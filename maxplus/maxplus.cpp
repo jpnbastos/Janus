f149a00a42e09bd712ff3324567d6a01ed68d3f1
Jbastos <j.p.nogueira.bastos@tue.nl>
2018-05-21 16:29:51 +0200

    new maxplus library

---
+++ maxplus/maxplus.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bastos on 21/05/2018.
+//
+
+#include "maxplus.h"
---
+++ maxplus/maxplus.h
@@ -0,0 +1,14 @@
+//
+// Created by Bastos on 21/05/2018.
+//
+
+#ifndef JANUS_MAXPLUS_H
+#define JANUS_MAXPLUS_H
+
+
+class maxplus {
+
+};
+
+
+#endif //JANUS_MAXPLUS_H
